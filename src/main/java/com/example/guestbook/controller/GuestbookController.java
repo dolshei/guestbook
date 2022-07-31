376637defa9f599f4cc346359e8590407b94da88
LEE.CHANG.HUN <dolshei@gmail.com>
2022-07-31 17:03:54 +0900

    thymeleaf를 이용한 리스트, 등록 상세구현

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.GuestbookDTO;
 import com.example.guestbook.dto.PageRequestDTO;
 import com.example.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor    // 자동 주입
@@ -15,6 +18,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GuestbookController {
 
     private final GuestbookService service;
+
+    @GetMapping("/register")
+    public void register() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+
+        // 새로 추가된 Entity의 번호
+        Long gno = service.register(dto);
+
+        // addFlashAttribute() : 단 한번만 데이터를 전달하는 용도록 사용한다.
+        // redirectAttributes : 한 번만 화면에서 "msg"라는 이름의 변수를 사용할 수 있도록 처리
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
+    }
 
     @GetMapping("/")
     public String index() {
