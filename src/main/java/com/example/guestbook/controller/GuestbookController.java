f1165662f92b6ad2c03ad98c97d9bb201e9bd008
LEE.CHANG.HUN <dolshei@gmail.com>
2022-07-31 16:44:43 +0900

    thymeleaf를 이용한 리스트 구현

--- src/main/java/com/example/guestbook/controller/GuestbookController.java
+++ src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -1,15 +1,28 @@
 package com.example.guestbook.controller;
 
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor    // 자동 주입
 @RequestMapping("/guestbook")
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(@ModelAttribute PageRequestDTO pageRequestDTO, Model model) {
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 }
--- src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
@@ -30,8 +30,13 @@ public class GuestbookServiceTests {
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
         PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
 
+        /*
+        // makePageList() 가 만들어지기 전에 테스트 사용
         for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
             System.out.println(guestbookDTO);
         }
+        */
+
+        System.out.println(resultDTO);
     }
 }
